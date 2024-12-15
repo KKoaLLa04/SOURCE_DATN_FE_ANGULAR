d59af0b3bddbf3937c9f6b6c7e8723f43e79381f
KKoaLLa04 <ndkdzvl@gmail.com>
2024-12-15 23:37:07 +0700

    create branch

--- src/app/modules/layout_staff/statistic-attendance/statistic-attendance.component.ts
+++ src/app/modules/layout_staff/statistic-attendance/statistic-attendance.component.ts
@@ -56,7 +56,6 @@ export class StatisticAttendanceComponent implements OnInit {
     }
     this.statisticAttendanceSerivce.getListStatisticAttendance(dataRequest).subscribe((res: any) => {
       this.dataList = res;
-      console.log(res)
       this.globalStore.isLoading = false;
     }, (err) =>{
       this.showMessageSerivce.error(err);
